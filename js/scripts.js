75af9be7f55a4cbb470a7deecff3fe3118ac32fb
Witty Chang <wcc1213@gmail.com>
2017-06-16 14:00:39 -0700

    Add scripts for output and initial formula for computation of order cost.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,51 @@
-// var pizzaOrder = {
-//    pizzaSize
-//    selectedToppings
-// }
-function pizzaOrder(size, toppings) {
-  this.size = size;
+//business logic
+function Pizza(size, toppings) {
+  this.size = pizzaSize;
   this.toppings = toppings;
 }
 
-var custOrder = new pizzaOrder();
+// function orderToppings(topping){
+//   this.topping = []
+//}
+//var custOrder = new pizzaOrder();
 
-var pizzaSize = $("#pizzaSize").val();
+var size = $("#pizzaSize").val();
+var doughCost;
+var toppingQty = toppingArray.length;
+var toppingCost = (toppingQty * 2);
+
+//formula to compute cost
+function costCompute(doughCost, toppingCost) {
+  if (size === 1) {
+    doughCost = 9;
+  } else if (size === 2) {
+    doughCost = 13;
+  } else if (size === 3) {
+    doughCost = 17;
+  } else {
+    alert('Please select a size for your pizza.');
+  }
+
+  var totalCost = (doughCost + toppingCost);
+    return totalCost
+}
 
 
 //user interface logic
 $(document).ready(function(){
   $("form#pizzaOrder").submit(function(event){
     event.preventDefault();
+    $("#finalOrder").show();
+    var name = $("input#customerName").val();
+    var pizzaSize = $("#pizzaSize").val();
+    var toppingArray = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      toppingArray.push($(this).val());
+    })
+
+    $("#custThank").append(name);
+    $("#finalOrder").append("<li>" + pizzaSize + "pizza with toppings of " + toppingArray.join(", ") + "for a total cost of" + "<br>")
+    
+
   })
 })
