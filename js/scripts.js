a76a295d3992a57e2379234411f2837206fbe4fa
Witty Chang <wcc1213@gmail.com>
2017-06-16 16:41:00 -0700

    Minor tweak for scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
 //business logic
 function Pizza(size, toppings) {
   this.size = pizzaSize;
-  this.topping = toppings;
+  this.toppings = toppings;
 }
 
+var totalCost = 0;
 var toppingArray = [];
 var toppingQty = toppingArray.length;
 
 //formula to compute cost
 Pizza.prototype.costCompute = function() {
 //pizza size select
-  var doughCost;
-  if (size === "small") {
-    doughCost = 9;
-  } else if (size === "medium") {
-    doughCost += 13;
+  if (this.size === "small") {
+    totalCost = (this.toppings.length + 9);
+  } else if (this.size === "medium") {
+    totalCost = (this.toppings.length + 13);
   } else if (size === "large") {
-    doughCost += 17;
+    totalCost = (this.toppings.length + 17);
   } else if (size === 0) {
     alert('Please select a size for your pizza.');
   }
-  var toppingQty = toppingArray.length;
-  var toppingCost = (toppingQty * 2);
-  totalCost = (doughCost + toppingCost);
-    return totalCost;
+  return totalCost;
 }
 
 //user interface logic
@@ -34,13 +31,16 @@ $(document).ready(function(){
     $("#orderConfirm").show();
     var name = $("input#customerName").val();
     var pizzaSize = $("#pizzaSize").val();
+
     $("input:checkbox[name=topping]:checked").each(function(){
       var toppingSelect = $(this).val();
       toppingArray.push(toppingSelect);
     })
-    var toppingQty = toppingArray.length;
-    var toppingCost = (toppingQty * 2);
-    var totalCost = (doughCost + toppingCost);
+    // var toppingQty = toppingArray.length;
+    // var toppingCost = (toppingQty * 2);
+
+    var newOrder = new Pizza(size, toppings);
+    finalTotal = newPizza.costCompute();
 
     $("#custThank").append(name);
     $("#finalOrder").append("<li>" + "A " + pizzaSize + " pizza with toppings of " + toppingArray.join(", ") + "." + "<br>");
