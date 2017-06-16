e0e85244f42783e69e722ada963e8e9716744f4e
Witty Chang <wcc1213@gmail.com>
2017-06-16 11:51:36 -0700

    Add options for pizza size and toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+// var pizzaOrder = {
+//    pizzaSize
+//    selectedToppings
+// }
+function pizzaOrder(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+var custOrder = new pizzaOrder();
+
 var pizzaSize = $("#pizzaSize").val();
+
+
+//user interface logic
+$(document).ready(function(){
+  $("form#pizzaOrder").submit(function(event){
+    event.preventDefault();
+  })
+})
