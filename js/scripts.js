0c0de69109b0eafc6d733063ed1f8fd9f972be72
Witty Chang <wcc1213@gmail.com>
2017-06-18 12:31:34 -0700

    Tweak scripts to show order cost.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,88 @@
-//business logic
-function Pizza(size, toppings) {
-  this.size = pizzaSize;
-  this.toppings = toppings;
-}
-
-var totalCost = 0;
-var toppingArray = [];
-var toppingQty = toppingArray.length;
-
-//formula to compute cost
-Pizza.prototype.costCompute = function() {
-//pizza size select
-  if (this.size === "small") {
-    totalCost = (this.toppings.length + 9);
-  } else if (this.size === "medium") {
-    totalCost = (this.toppings.length + 13);
-  } else if (size === "large") {
-    totalCost = (this.toppings.length + 17);
-  } else if (size === 0) {
-    alert('Please select a size for your pizza.');
-  }
-  return totalCost;
-}
-
 //user interface logic
 $(document).ready(function(){
+  var order = new Order();
   $("form#pizzaOrder").submit(function(event){
     event.preventDefault();
-    debugger;
-    $("#orderConfirm").show();
+debugger;
     var name = $("input#customerName").val();
     var pizzaSize = $("#pizzaSize").val();
+
+    $("#orderConfirm").show();
 
     $("input:checkbox[name=topping]:checked").each(function(){
       var toppingSelect = $(this).val();
       toppingArray.push(toppingSelect);
     })
-    // var toppingQty = toppingArray.length;
-    // var toppingCost = (toppingQty * 2);
-
-    var newOrder = new Pizza(size, toppings);
-    finalTotal = newPizza.costCompute();
 
     $("#custThank").append(name);
     $("#finalOrder").append("<li>" + "A " + pizzaSize + " pizza with toppings of " + toppingArray.join(", ") + "." + "<br>");
-    $("#totalCost").append(totalCost);
+    $("#totalCost").text(order.costCompute());
   })
 })
+
+//business logic
+function Order() {
+  this.name;
+  this.pizzas = [];
+}
+
+function Pizza(size, toppings) {
+  this.size = pizzaSize;
+  this.toppings = [];
+}
+
+var toppingArray = [];
+var toppingQty = toppingArray.length;
+
+//formula to compute cost
+Order.prototype.costCompute = function() {
+  var cost = 0;
+  this.pizzas.forEach(function(pizza) {
+    if (pizza.size === "small") {
+      cost += 9;
+    } else if (pizza.size === "medium") {
+      cost += 13;
+    } else if (size === "large") {
+      cost += 17;
+    }
+
+    pizza.toppings.forEach(function(topping) {
+      if (topping === "mozzarella cheese") {
+        cost += 2;
+      }
+      if (topping === "pepperoni") {
+        cost += 2;
+      }
+      if (topping === "green bell peppers") {
+        cost += 2;
+      }
+      if (topping === "black olives") {
+        cost += 2;
+      }
+      if (topping === "mushrooms") {
+        cost += 2;
+      }
+      if (topping === "fresh onions") {
+        cost += 2;
+      }
+      if (topping === "italian sausage") {
+        cost += 2;
+      }
+      if (topping === "canadian bacon") {
+        cost += 2;
+      }
+      if (topping === "anchovies") {
+        cost += 2;
+      }
+    });
+  });
+  return cost;
+}
+
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
